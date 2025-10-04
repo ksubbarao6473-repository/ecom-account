68eb947778349c246629417450325d60a0cb8a1c
Subbarao <ksrsubba432107@gmail.com>
2025-10-04 19:46:09 +0530

    initial commit changes for save and all endpoints

--- account/src/main/java/com/ecommerce/app/account/controller/CustomerController.java
+++ account/src/main/java/com/ecommerce/app/account/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.ecommerce.app.account.controller;
 
 import com.ecommerce.app.account.entity.Customer;
 import com.ecommerce.app.account.exception.DatabaseException;
+import com.ecommerce.app.account.model.CustomerDto;
 import com.ecommerce.app.account.service.CustomerServiceImpl;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -16,19 +18,21 @@ import java.util.List;
 public class CustomerController {
 
     private final CustomerServiceImpl customerService;
-
+    UUID uuid = UUID.randomUUID();
+    String uuidString = uuid.toString();
     public CustomerController(CustomerServiceImpl customerService) {
         this.customerService = customerService;
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Customer> saveCustomer( @RequestBody @Valid Customer customer) {
-        Customer result = null;
+    public ResponseEntity<CustomerDto> saveCustomer(@RequestBody @Valid CustomerDto customerDto) {
+        CustomerDto result = null;
             try {
-                result = customerService.saveCustomer(customer);
+                result = customerService.saveCustomer(customerDto);
                 log.info("Customer saved successfully: {}", result);
-                return ResponseEntity.ok(result);
+                return new ResponseEntity<>(result, HttpStatus.CREATED);
             } catch (DatabaseException e) {
+                log.error("Error saving customer", e);
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
     }
@@ -39,12 +43,39 @@ public class CustomerController {
         try {
             customers = customerService.getAllCustomers();
             log.info("Fetched {} customers", customers.size());
-            return ResponseEntity.ok(customers);
+            return new ResponseEntity<>(customers,HttpStatus.OK);
         } catch (DatabaseException e) {
+            log.error("Error fetching customers", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/get")
+    public ResponseEntity<Customer> getCustomerById(@RequestParam("email") String email) {
+        Customer customer = null;
+        try {
+             customer = customerService.getByEmail(email);
+            if (customer == null) {
+                log.warn("Customer with email {} not found", email);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+            log.info("Fetched customer: {}", customer);
+            return ResponseEntity.ok(customer);
+        } catch (DatabaseException e) {
+            log.error("Error fetching customer with email {0}, error message {1}" + email, e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-
+    @PutMapping("/{id}")
+    public  ResponseEntity<Customer> updateCustomer(@PathVariable Long id) {
+        Customer updatedCustomer = null;
+        try {
+            updatedCustomer = customerService.updateCustomer(id);
+            log.info("Customer updated successfully: {}", updatedCustomer);
+            return ResponseEntity.ok(updatedCustomer);
+        } catch (DatabaseException e) {
+            log.error("Error updating customer", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 
 }
--- account/src/main/java/com/ecommerce/app/account/service/CustomerServiceImpl.java
+++ account/src/main/java/com/ecommerce/app/account/service/CustomerServiceImpl.java
@@ -3,25 +3,51 @@ package com.ecommerce.app.account.service;
 import com.ecommerce.app.account.dao.CustomerRepository;
 import com.ecommerce.app.account.entity.Customer;
 import com.ecommerce.app.account.exception.DatabaseException;
+import com.ecommerce.app.account.model.CustomerDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerServiceImpl {
 
+    private final ModelMapper modelMapper;
+
     private final CustomerRepository customerRepository;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
         this.customerRepository = customerRepository;
     }
 
-    public Customer saveCustomer(Customer customer) throws DatabaseException {
-        return customerRepository.save(customer);
+    private Customer DtoToCustomer(CustomerDto customerDto)
+    {
+        return modelMapper.map(customerDto, Customer.class);
+    }
+
+    private CustomerDto CustomerToDto(Customer customer)
+    {
+        return modelMapper.map(customer, CustomerDto.class);
+    }
+
+    public CustomerDto saveCustomer(CustomerDto customerDto) throws DatabaseException {
+        Customer customer = DtoToCustomer(customerDto);
+        Customer result = customerRepository.save(customer);
+        return CustomerToDto(result);
     }
     public List<Customer> getAllCustomers() throws DatabaseException {
         return customerRepository.findAll();
+    }
+
+    public Customer getByEmail(String email) throws DatabaseException {
+        return customerRepository.findByEmail(email.replaceAll("\\n", "")).orElse(null);
+    }
+
+    public Customer updateCustomer(Long id) throws DatabaseException {
+
+
+        return null;
     }
 
 }
